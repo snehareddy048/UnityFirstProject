97ffb88b591a2f3c74b61e21676b4622fe49af80
sneha.yeluguri@capitalone.com <sneha.yeluguri@capitalone.com>
2016-07-12 14:00:12 -0400

    first commit

---
+++ Assets/Scripts/Generate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Generate : MonoBehaviour {
+	public GameObject rocks;
+	int score = 0;
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("CreateObstacle", 1f, 1.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Update is called once per frame
+	void OnGUI () 
+	{
+		GUI.color = Color.black;
+		GUILayout.Label(" Score: " + score.ToString());
+	}
+	void CreateObstacle()
+	{
+		Instantiate(rocks);
+		score++;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player: MonoBehaviour {
+
+	// The force which is added when the player jumps
+	// This can be changed in the Inspector window
+	public Vector2 jumpForce = new Vector2(0, 300);
+	 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Jump
+		if (Input.GetKeyUp("space"))
+		{
+			GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+			GetComponent<Rigidbody2D>().AddForce(jumpForce);
+		}
+		// Die by being off screen
+		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+		if (screenPosition.y > Screen.height || screenPosition.y < 0)
+		{
+			Die();
+		}
+	}
+	// Die by collision
+	void OnCollisionEnter2D(Collision2D other)
+	{
+		Die();
+	}
+
+	void Die()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+}
